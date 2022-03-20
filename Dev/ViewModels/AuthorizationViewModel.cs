0624f0ef151097f11bcffab6d9b7fae047c6e7fa
ezprojectsru <100280713+ezprojectsru@users.noreply.github.com>
2022-03-21 00:32:30 +0300

    init project
    init project files

---
+++ Dev/ViewModels/AuthorizationViewModel.cs
@@ -0,0 +1,95 @@
+﻿using DevExpress.Mvvm;
+using ReportGenerator.Services;
+using System.Windows;
+using System.Windows.Input;
+using ReportGenerator.DataBase.Controls;
+using ReportGenerator.DataBase.Models;
+using ReportGenerator.Views;
+
+using System.Data.SqlClient;
+
+namespace ReportGenerator.ViewModels
+{
+    /// <summary>
+    /// Класс ViewModel для страницы авторизации и проверки подключения к серверу
+    /// </summary>
+    public class AuthorizationViewModel : BindableBase
+    {
+        private bool isConnected = false;
+        public string ConnectionStatus { get; set; } = "Подключение к Серверу отсутствует!";
+        public string Login { get; set; }
+        public string Password { get; set; }
+        
+        public AuthorizationViewModel()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// Процедура инициализации. Проверка подключения к серверу
+        /// </summary>
+        private void Init()
+        {
+            DbConnection db = new DbConnection();
+            SqlConnection conn = db.ConnectOpen();
+
+            if (conn != null)
+            {
+
+                isConnected = true;
+                ConnectionStatus = "";
+                db.ConnectClose();
+            }
+            else
+            {
+                ConnectionStatus = "Подключение к Серверу отсутствует!";
+            }
+        }   
+
+        /// <summary>
+        /// Команда для проверки введеных Логина и Пароля. При успехе переадресовывает на основную страницу приложения.
+        /// </summary>
+        public ICommand LoginUser => new DelegateCommand(() =>
+        {
+            User user = UserControl.GetUser(Login);
+            if (user != null)
+            {
+                if (Password == user.password)
+                {                      
+
+                    MainWindow mainWindow = new MainWindow();
+                    SessionUser sessionUser = new SessionUser(user);
+                    MessageService.Send(sessionUser);
+                    mainWindow.Show();
+                    Application.Current.MainWindow.Close();
+                }
+                else 
+                {
+                    MessageBox.Show("Вы ввели неправильный Логин или Пароль!");
+                }
+                
+            }
+            else
+            { 
+                MessageBox.Show("Вы ввели неправильный Логин или Пароль!"); 
+            }            
+            
+            
+        }, ()=> !string.IsNullOrWhiteSpace(Login) && !string.IsNullOrWhiteSpace(Password)&& isConnected);
+
+        /// <summary>
+        /// Команда для открытия окна настроек подключения к серверу
+        /// </summary>
+        public ICommand OpenSettingsWindow => new DelegateCommand(() =>
+        {             
+
+            SettingsWindow settingsWindow = new SettingsWindow();
+            if (settingsWindow.ShowDialog() == true)
+            {
+                Init();
+            }
+
+        });
+
+    }
+}
---
+++ Dev/ViewModels/SettingsWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using DevExpress.Mvvm;
+using ReportGenerator.Services;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ReportGenerator.ViewModels
+{
+    public class SettingsWindowViewModel : BindableBase
+    {
+        /// <summary>
+        /// Класс ViewModels для окна с настройка подключения к серверу
+        /// </summary>
+        public ConnectConfig Config { get; set; }
+        public string Title { get; set; }
+
+
+        public SettingsWindowViewModel()
+        {
+            try
+            {
+                Title = "Настройки подключения";
+                string data = File.ReadAllText("settings.json");
+                Config = JsonSerializer.Deserialize<ConnectConfig>(data);
+            }
+            catch
+            {
+                Config.Server = "";
+                Config.DbName = "";
+                Config.Username = "";
+                Config.Password = "";
+            }
+            
+        }
+
+        /// <summary>
+        /// Команда для созранения настроек
+        /// </summary>
+        public ICommand SaveSettings => new DelegateCommand<object>((currentWindow) =>
+        {
+            ConnectConfig config = new ConnectConfig()
+            {
+                Server = Config.Server,
+                DbName = Config.DbName,
+                Username = Config.Username,
+                Password = Config.Password
+            };
+            string configJson = JsonSerializer.Serialize(config, typeof(ConnectConfig));
+            StreamWriter file = File.CreateText("settings.json");
+            file.WriteLine(configJson);
+            file.Close();
+
+            Window wnd = currentWindow as Window;
+            wnd.DialogResult = true;
+            wnd.Close();
+
+
+
+        });
+
+        
+
+    }
+}
