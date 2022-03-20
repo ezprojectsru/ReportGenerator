3d478616de5983443fb2f7c49f6157616ae7a1fc
ezprojectsru <100280713+ezprojectsru@users.noreply.github.com>
2022-03-21 00:31:54 +0300

    init project
    init project files

---
+++ Dev/Services/MessageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ReportGenerator.Services
+{
+    /// <summary>
+    /// Класс для передачи объектов между ViewModel
+    /// </summary>
+    public static class MessageService
+    {
+        public static event Action<object> Bus;
+        public static void Send(object data)
+            => Bus?.Invoke(data);
+    }
+}
---
+++ Dev/Services/PageNavigationService.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Windows.Controls;
+
+namespace ReportGenerator.Services
+{
+    /// <summary>
+    /// Класс сервиса навигации по страницам
+    /// </summary>
+    public class PageNavigationService
+    {
+        public event Action<Page> OnPageChanged;
+        
+        public void Navigate(Page page)
+        {
+            OnPageChanged?.Invoke(page);            
+        }        
+    }
+}
