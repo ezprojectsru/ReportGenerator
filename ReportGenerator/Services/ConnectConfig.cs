e4788b35e09bb718b6eef17b078828006b2bbae6
ezprojectsru <100280713+ezprojectsru@users.noreply.github.com>
2022-03-21 20:57:33 +0300

    init
    init

---
+++ ReportGenerator/Services/ConnectConfig.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ReportGenerator.Services
+{
+    /// <summary>
+    /// Класс для сериализации настроек подключения к серверу
+    /// </summary>
+    public class ConnectConfig
+    {
+        public string Server { get; set; }
+        public string DbName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }        
+    }
+}
