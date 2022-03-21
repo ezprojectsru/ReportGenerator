cae07ba73a6ef6a9510916431382e9a678048f1e
ezprojectsru <100280713+ezprojectsru@users.noreply.github.com>
2022-03-21 20:58:35 +0300

    init
    init

---
+++ ReportGenerator/Views/Authorization.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+
+namespace ReportGenerator.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+    }
+}
