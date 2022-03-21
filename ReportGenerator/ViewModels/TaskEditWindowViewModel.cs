10f26e2481f09f73551621ca73a8f388692932f8
ezprojectsru <100280713+ezprojectsru@users.noreply.github.com>
2022-03-21 11:33:22 +0300

    init project
    init project

---
+++ ReportGenerator/ViewModels/TaskEditWindowViewModel.cs
@@ -0,0 +1,145 @@
+﻿using DevExpress.Mvvm;
+using ReportGenerator.DataBase.Controls;
+using ReportGenerator.DataBase.Models;
+using ReportGenerator.Services;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ReportGenerator.ViewModels
+{
+    public class TaskEditWindowViewModel : BindableBase
+    {
+        /// <summary>
+        /// Класс ViewModel для окна редактирования задач
+        /// </summary>
+        public string Title { get; set; }
+        private Task _task;
+        private int _departamentId;
+        private int _responsibleId;
+        public List<string> Types { get; set; }
+        private int Id;
+        public string Name { get; set; }   
+        public int PlanId { get; set; }
+        public string Priority { get; set; }
+        public string Type { get; set; }
+        public string Intensity { get; set; }
+        public string StartCompletion { get; set; }
+        public string PlanCompletion { get; set; }
+        public string Comment { get; set; }
+
+        public TaskEditWindowViewModel()
+        {
+            Title = "Создание задачи";            
+            MessageService.Bus += Receive;            
+        }
+
+        /// <summary>
+        /// Принимает объекты, необходимые для работы класса.       
+        /// Task - задача со страницы Планов
+        /// INT - нулевый int, является сигналом, что мы не редактируем существующую задачу, я добавляем новую
+        /// </summary> 
+        private void Receive(object dataReceive)
+        {
+            if (dataReceive is Task data)
+            {
+                _task = data;
+                setStrings();                
+                MessageService.Bus -= Receive;
+            }
+
+            if (dataReceive is int id)
+            {
+                clearStrings(id);
+                MessageService.Bus -= Receive;
+            }
+        }
+
+        /// <summary>
+        /// Инициализация combobox'ов
+        /// </summary>        
+        private void setTypes(int planId)
+        {
+            Types = new List<string>();
+            _responsibleId = PlanControl.GetResponsibleIdByPlanId(planId);
+            _departamentId = UserControl.GetDepartamentIdById(_responsibleId);            
+            Types = TaskTypeControl.GetTaskShortNamesListByDepartamentId(_departamentId);
+        }
+
+        /// <summary>
+        /// Инициализация полей формы в случае с редактированием существующей задачи
+        /// </summary>
+        private void setStrings()
+        {
+            if (_task != null)
+            {
+                Id = _task.id;
+                Name = _task.name;
+                PlanId = _task.planId;
+                Priority = _task.priority.ToString();
+                Type = _task.type;
+                Intensity = _task.intensity.ToString();
+                StartCompletion = _task.startCompletion.ToString();
+                PlanCompletion = _task.planCompletion.ToString();
+                Comment = _task.comment;
+                Title = "Редактирование задачи";
+                setTypes(PlanId);
+            }
+        }
+
+        /// <summary>
+        /// Инициализация полей формы в случае с созданием новой задачи
+        /// </summary>
+        private void clearStrings(int id)
+        {
+            Id = 0;
+            Name = "";
+            PlanId = id;
+            Priority = "1";
+            Type = "";
+            Intensity = "";
+            StartCompletion = "";
+            PlanCompletion = "";
+            Comment = "";
+            Title = "Создание задачи";
+            setTypes(PlanId);
+        }
+
+        /// <summary>
+        /// Команда для отправки результата работы окна
+        /// </summary>
+        public ICommand SendDialogResultTask => new DelegateCommand<object>((currentWindow) =>
+        {
+            if (!string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Intensity) && !string.IsNullOrWhiteSpace(StartCompletion) && !string.IsNullOrWhiteSpace(PlanCompletion))
+            {
+                try
+                {
+                    Task _resultTask = new Task();
+                    _resultTask.id = Id;
+                    _resultTask.name = Name;
+                    _resultTask.planId = PlanId;
+                    _resultTask.priority = Int32.Parse(Priority);
+                    _resultTask.type = Type;
+                    _resultTask.intensity = Int32.Parse(Intensity);
+                    _resultTask.startCompletion = Int32.Parse(StartCompletion);
+                    _resultTask.planCompletion = Int32.Parse(PlanCompletion);
+                    _resultTask.comment = Comment;
+
+                    MessageService.Send(_resultTask);
+                    Window wnd = currentWindow as Window;
+                    wnd.DialogResult = true;
+                    wnd.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Поля заполнены не корректно!", "Ошибка");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не все поля заполнены!", "Ошибка");
+            }
+        });
+    }
+}
---
+++ ReportGenerator/ViewModels/UserEditWindowViewModel.cs
@@ -0,0 +1,226 @@
+﻿using DevExpress.Mvvm;
+using ReportGenerator.DataBase.Controls;
+using ReportGenerator.DataBase.Models;
+using ReportGenerator.Services;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ReportGenerator.ViewModels
+{
+    /// <summary>
+    /// Класс ViewModel для окна редактирования пользователей
+    /// </summary>
+    public class UserEditWindowViewModel : BindableBase
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime СreateDate { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string DepartamentId { get; set; }
+        public string RoleId { get; set; }
+        public string SectorId { get; set; }
+        public string GroupId { get; set; }
+        private User _user;
+        public List<string> Departaments{ get; set; }
+        public string Departament { get; set; }
+        public List<string> Roles { get; set; }
+        public string Role { get; set; }
+        public List<string> Sectors { get; set; }
+        public string Sector { get; set; }
+        public List<string> Groups { get; set; }
+        public string Group { get; set; }
+        private string oldUsername;
+
+        public UserEditWindowViewModel()
+        {
+            Title = "Добавление пользователя";
+            MessageService.Bus += Receive;
+        }
+
+        /// <summary>
+        /// Принимает объекты, необходимые для работы класса.       
+        /// User - потльзователь со страницы Управления и настроек
+        /// INT - нулевый int, является сигналом, что мы не редактируем существующего пользователя, я добавляем нового
+        /// </summary>
+        private void Receive(object dataReceive)
+        {
+            if (dataReceive is User data)
+            {
+                _user = data;
+                setStrings();
+                MessageService.Bus -= Receive;
+            }
+
+            if (dataReceive is int id)
+            {
+                clearStrings();
+                MessageService.Bus -= Receive;
+            }
+        }
+
+        /// <summary>
+        /// Инициализация полей формы в случае с редактированием существующего пользователя
+        /// </summary>
+        private void setStrings()
+        {
+            if (_user != null)
+            {
+                Id = _user.id;
+                Username = _user.username;
+                oldUsername = Username;
+                Password = _user.password;
+                СreateDate = _user.create_date;
+                FullName = _user.fullName;
+                Email = _user.email;
+                DepartamentId = _user.departamentId.ToString();
+                RoleId = _user.roleId.ToString();
+                SectorId = _user.sectorId.ToString();
+                GroupId = _user.groupId.ToString();
+                Title = "Редактирование пользователя";
+                Departaments = new List<string>();
+                Departaments = DepartamentControl.GetAllNameDepartaments();
+                Departament = DepartamentControl.GetNameById(_user.departamentId);
+                Roles = new List<string>();
+                Roles = RoleControl.GetAllNameRoles();
+                Role = RoleControl.GetNameById(_user.roleId);
+                Sectors = new List<string>();
+                Sectors = SectorControl.GetAllNameSectors();
+                Sector = SectorControl.GetNameById(_user.sectorId);
+                Groups = new List<string>();
+                Groups = GroupControl.GetAllNameGroups();
+                Group = GroupControl.GetNameById(_user.groupId);
+            }
+        }
+
+        /// <summary>
+        /// Инициализация полей формы в случае с добавлением нового пользователя
+        /// </summary>
+        private void clearStrings()
+        {
+            Id = 0;
+            Username = "";
+            Password = "";
+            СreateDate = DateTime.Now;
+            FullName = "";
+            Email = "";
+            DepartamentId = "";
+            RoleId = "";
+            SectorId = "";
+            GroupId = "";
+            Title = "Добавление пользователя";
+            Departaments = new List<string>();
+            Departaments = DepartamentControl.GetAllNameDepartaments();
+            Roles = new List<string>();
+            Roles = RoleControl.GetAllNameRoles();
+            Sectors = new List<string>();
+            Sectors = SectorControl.GetAllNameSectors();
+            Groups = new List<string>();
+            Groups = GroupControl.GetAllNameGroups();
+        }
+
+
+        /// <summary>
+        /// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        /// 
+        ///      ТУТ ВСЕ ПЕРЕДЕЛАЮ !!!!
+        ///      
+        ///      в 00.00 нашел случайно багу, но сил совсем не осталось :))
+        ///      Пофиксил пока так...
+        ///      
+        /// </summary>
+        public ICommand SendDialogResultUser => new DelegateCommand<object>((currentWindow) =>
+        {
+            if (!string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password) && !string.IsNullOrWhiteSpace(FullName) && !string.IsNullOrWhiteSpace(Email))
+            {
+                if (Id == 0)
+                {
+                    if (!UserControl.ExistsUserName(Username))
+                    {
+
+                        try
+                        {
+                            int departamentId = DepartamentControl.GetIddByName(Departament);
+                            int roleId = RoleControl.GetIddByName(Role);
+                            int sectorId = SectorControl.GetIddByName(Sector);
+                            int groupId = GroupControl.GetIddByName(Group);
+                            User _resultUser = new User(Id, Username, Password, СreateDate, FullName, Email, departamentId, roleId, sectorId, groupId);
+                            MessageService.Send(_resultUser);
+                            Window wnd = currentWindow as Window;
+                            wnd.DialogResult = true;
+                            wnd.Close();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Поля заполнены не корректно!", "Ошибка");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Такое имя уже существует!", "Ошибка");
+                    }
+                }
+                else
+                {
+                    if (oldUsername != Username)
+                    {
+                        if (!UserControl.ExistsUserName(Username))
+                        {
+                            try
+                            {
+
+                                int departamentId = DepartamentControl.GetIddByName(Departament);
+                                int roleId = RoleControl.GetIddByName(Role);
+                                int sectorId = SectorControl.GetIddByName(Sector);
+                                int groupId = GroupControl.GetIddByName(Group);
+                                User _resultUser = new User(Id, Username, Password, СreateDate, FullName, Email, departamentId, roleId, sectorId, groupId);
+                                MessageService.Send(_resultUser);
+                                Window wnd = currentWindow as Window;
+                                wnd.DialogResult = true;
+                                wnd.Close();
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Поля заполнены не корректно!", "Ошибка");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Такое имя уже существует!", "Ошибка");
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+
+                            int departamentId = DepartamentControl.GetIddByName(Departament);
+                            int roleId = RoleControl.GetIddByName(Role);
+                            int sectorId = SectorControl.GetIddByName(Sector);
+                            int groupId = GroupControl.GetIddByName(Group);
+                            User _resultUser = new User(Id, Username, Password, СreateDate, FullName, Email, departamentId, roleId, sectorId, groupId);
+                            MessageService.Send(_resultUser);
+                            Window wnd = currentWindow as Window;
+                            wnd.DialogResult = true;
+                            wnd.Close();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Поля заполнены не корректно!", "Ошибка");
+                        }
+                    }
+                }
+                
+                
+            }
+            else
+            {
+                MessageBox.Show("Не все поля заполнены!", "Ошибка");
+            }
+        });
+    }
+}
