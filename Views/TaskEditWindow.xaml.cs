141bc1d3958d96f570e80ef808876cc6fca90e8d
ezprojectsru <100280713+ezprojectsru@users.noreply.github.com>
2022-03-21 20:58:57 +0300

    init
    init

---
+++ Views/TaskEditWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReportGenerator.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskEditWindow.xaml
+    /// </summary>
+    public partial class TaskEditWindow : Window
+    {
+        
+        public TaskEditWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+
+    }
+}
