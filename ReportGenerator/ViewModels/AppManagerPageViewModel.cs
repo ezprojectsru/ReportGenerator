39f45bc9ba4e7d754737b0411f0f67ce1e3e820a
ezprojectsru <8492303@gmail.com>
2022-03-23 17:42:13 +0300

    Вынес все классы в отдельные файлы

--- ReportGenerator/ViewModels/AppManagerPageViewModel.cs
+++ ReportGenerator/ViewModels/AppManagerPageViewModel.cs
@@ -3,53 +3,12 @@ using ReportGenerator.DataBase.Controls;
 using ReportGenerator.DataBase.Models;
 using ReportGenerator.Services;
 using ReportGenerator.Views;
-using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 
 namespace ReportGenerator.ViewModels
-{
-    /// <summary>
-    /// Вспомогательный клас для конвертации полей с ID в названия полей. Будет упразнен после написания конвертора.
-    /// </summary>
-    public class ItemUser
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public DateTime Сreate_Date { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }        
-        public string Departament { get; set; }        
-        public string Role { get; set; }        
-        public string Sector { get; set; }        
-        public string Group { get; set; }
-
-        private DepartamentControl _departamentControl = new DepartamentControl();
-        private GroupControl _groupControl = new GroupControl();
-        private RoleControl _roleControl = new RoleControl();
-        private SectorControl _sectorControl = new SectorControl();
-
-        public ItemUser()
-        {
-        }
-
-            public ItemUser(User user)
-        {
-            Id = user.id;
-            Username = user.username;
-            Password = user.password;
-            Сreate_Date = user.create_date;
-            FullName = user.fullName;
-            Email = user.email;
-            
-            Departament = _departamentControl.GetNameById(user.departamentId);
-            Role = _roleControl.GetNameById(user.roleId);
-            Sector = _sectorControl.GetNameById(user.sectorId);
-            Group = _groupControl.GetNameById(user.groupId);
-        }
-    }
+{   
 
     /// <summary>
     /// Класс ViewModel для страницы настроек и управления
